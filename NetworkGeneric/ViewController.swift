bf8e7ea6133076314318f3b780a3388ee801b199
Vadim Gamzaev <Vadosbox@gmail.com>
2022-05-02 15:22:18 +0300

    Initial Commit

---
+++ NetworkGeneric/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NetworkGeneric
+//
+//  Created by Вадим Гамзаев on 02.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
